da812fea3d99ba5997ff83faf33c87c8bd13911e
Your Name <you@example.com>
2026-01-04 01:46:59 +0100

    [REFACTOR]: cleaning code for the Office DTOS

--- src/main/java/com/spx/inventory_management/dto/OfficeResponseDTO.java
+++ src/main/java/com/spx/inventory_management/dto/OfficeResponseDTO.java
@@ -5,10 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class OfficeResponseDTO {
+public class OfficeResponseDTO implements Serializable {
 
     private Long id;
     private String officeName;
