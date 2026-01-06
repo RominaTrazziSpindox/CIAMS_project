56452b1f55dc736613541de4bed8266654159e29
Your Name <you@example.com>
2026-01-06 22:44:59 +0100

    [FEATURE]: added Asset DTO and Mapper

--- src/main/java/com/spx/inventory_management/dto/AssetRequestDTO.java
+++ src/main/java/com/spx/inventory_management/dto/AssetRequestDTO.java
@@ -1,4 +1,26 @@
 package com.spx.inventory_management.dto;
 
-public class AssetRequestDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+@Data
+@NoArgsConstructor
+public class AssetRequestDTO implements Serializable {
+
+    @NotBlank(message = "Serial number cannot be blank")
+    private String serialNumber;
+
+    private LocalDate purchaseDate;
+
+    @NotNull(message = "Asset Type ID is required")
+    private Long assetTypeId;
+
+    @NotNull(message = "Office ID is required")
+    private Long officeId;
 }
--- src/main/java/com/spx/inventory_management/mapper/AssetMapper.java
+++ src/main/java/com/spx/inventory_management/mapper/AssetMapper.java
@@ -1,4 +1,24 @@
 package com.spx.inventory_management.mapper;
 
+import com.spx.inventory_management.dto.AssetRequestDTO;
+import com.spx.inventory_management.dto.AssetResponseDTO;
+import com.spx.inventory_management.models.Asset;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface AssetMapper {
+
+    // DTO -> Entity
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "office", ignore = true)
+    @Mapping(target = "assetType", ignore = true)
+    Asset toEntity(AssetRequestDTO dto);
+
+
+    // Entity -> DTO
+    @Mapping(source = "office.name", target = "officeName")
+    @Mapping(source = "assetType.assetTypeName", target = "assetTypeName")
+    @Mapping(source = "assetType.assetTypeDescription", target = "assetTypeDescription")
+    AssetResponseDTO toResponseDTO(Asset asset);
 }
