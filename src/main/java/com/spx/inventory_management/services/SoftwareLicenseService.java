10e88efd31bf651d32bac5e3a9e82ec2028a8782
Your Name <you@example.com>
2026-01-07 08:30:47 +0100

    [FEATURE] added installation\uninstallation\audit and max expired soon function of Software license entity

--- src/main/java/com/spx/inventory_management/services/SoftwareLicenseService.java
+++ src/main/java/com/spx/inventory_management/services/SoftwareLicenseService.java
@@ -1,6 +1,8 @@
 package com.spx.inventory_management.services;
 
+import com.spx.inventory_management.models.Asset;
 import com.spx.inventory_management.models.SoftwareLicense;
+import com.spx.inventory_management.repositories.AssetRepository;
 import com.spx.inventory_management.repositories.SoftwareLicenseRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
@@ -8,7 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Slf4j
@@ -16,6 +20,9 @@ public class SoftwareLicenseService {
 
     @Autowired
     private SoftwareLicenseRepository softwareLicenseRepository;
+
+    @Autowired
+    private AssetRepository assetRepository;
 
     // ==========================================================
     // READ OPERATIONS
@@ -96,5 +103,103 @@ public class SoftwareLicenseService {
         // Proceed with deletion.
         log.info("Deleting Software License. id={}", id);
         softwareLicenseRepository.deleteById(id);
+    }
+
+    // ==========================================================
+    // SOFTWARE INSTALLATION & COMPLIANCE
+    // ==========================================================
+
+    @Transactional
+    public SoftwareLicense installationSoftware(long licenseId, long assetId) {
+
+        // Check if the asset exists
+        Asset asset = assetRepository.findById(assetId).orElseThrow(() -> {
+            log.error("Installation failed. Asset not found. id={}", assetId);
+            return new EntityNotFoundException("Asset not found");
+        });
+
+        // Check if the software license exists
+        SoftwareLicense softwareLicense = softwareLicenseRepository.findById(licenseId).orElseThrow(() -> {
+            log.error("Installation failed. Software License not found. id={}", licenseId);
+            return new EntityNotFoundException("Software License not found");
+        });
+
+        // Check if the software license is not expired
+        if (softwareLicense.getExpirationDate().isBefore(LocalDate.now())) {
+            log.error("Installation failed. Software License expired. id={}", licenseId);
+            throw new IllegalStateException("Software License is expired");
+        }
+
+        // Check if the software has already this license installed (= same licenseId)
+        if (softwareLicense.getInstalledAssets().contains(asset)) {
+            log.error("Installation failed. License already installed. licenseId={}, assetId={}", licenseId, assetId);
+            throw new IllegalStateException("Software already installed on this asset");
+        }
+
+        // Check the number of installation
+        if (softwareLicense.getMaxInstallations() != null && softwareLicense.getInstalledAssets().size() >= softwareLicense.getMaxInstallations()) {
+            log.error("Installation failed. Number of maximum installations reached. licenseId={}", licenseId);
+            throw new IllegalStateException("Maximum number of installations reached!");
+        }
+
+        // Add the license to the asset
+        softwareLicense.getInstalledAssets().add(asset);
+
+        log.info("The Software License has been installed. licenseId={}, assetId={}", licenseId, assetId);
+
+        return softwareLicenseRepository.save(softwareLicense);
+    }
+
+    @Transactional
+    public SoftwareLicense uninstallSoftware(long licenseId, long assetId) {
+
+        // Check if the asset exists
+        Asset asset = assetRepository.findById(assetId).orElseThrow(() -> {
+            log.error("Uninstallation failed. Asset not found. id={}", assetId);
+            return new EntityNotFoundException("Asset not found");
+        });
+
+        // Check if the software license exists
+        SoftwareLicense softwareLicense = softwareLicenseRepository.findById(licenseId).orElseThrow(() -> {
+            log.error("Uninstallation failed. Software License not found. id={}", licenseId);
+            return new EntityNotFoundException("Software License not found");
+        });
+
+        // Check if the asset contains the specific software license
+        if (!softwareLicense.getInstalledAssets().contains(asset)) {
+            log.error("Uninstallatopm failed. Software not installed. licenseId={}, assetId={}",licenseId, assetId);
+            throw new IllegalStateException("This Software license is not installed on this asset");
+        }
+
+        // Remove the license to the asset
+        softwareLicense.getInstalledAssets().remove(asset);
+
+        log.info("Uninstalled Software License. licenseId={}, assetId={}", licenseId, assetId);
+
+        return softwareLicenseRepository.save(softwareLicense);
+    }
+
+    // Retrieve all the licenses owned by an asset
+    public Set<SoftwareLicense> getInstalledSoftwareByAsset(long assetId) {
+
+        Asset asset = assetRepository.findById(assetId).orElseThrow(() -> {
+            log.error("Audit failed. Asset not found. id={}", assetId);
+            return new EntityNotFoundException("Asset not found");
+        });
+
+        log.info("Audit software for Asset. assetId={}", assetId);
+
+        return asset.getSoftwareLicenses();
+    }
+
+    // Retrieve a list of the licenses that will be expired into 30 days
+    public List<SoftwareLicense> getLicensesExpiringSoon() {
+
+        LocalDate now = LocalDate.now();
+        LocalDate limit = now.plusDays(30);
+
+        log.info("Fetching software licenses expiring between {} and {}", now, limit);
+
+        return softwareLicenseRepository.findByExpirationDateBetween(now, limit);
     }
 }
