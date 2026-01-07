0deb71490fa83fe284a85c3c9af034d1babbb026
Your Name <you@example.com>
2026-01-07 05:22:15 +0100

    [FEATURE]: added methods in Asset repository and Office repository

--- src/main/java/com/spx/inventory_management/repositories/OfficeRepository.java
+++ src/main/java/com/spx/inventory_management/repositories/OfficeRepository.java
@@ -4,7 +4,11 @@ import com.spx.inventory_management.models.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OfficeRepository extends JpaRepository<Office, Long> {
+
+    Optional<Office> findByName(String name);
 
 }
