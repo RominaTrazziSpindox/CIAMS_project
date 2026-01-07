3b17e0e47fff8fccbed5ba287184d352e44aeb4b
Your Name <you@example.com>
2026-01-07 05:22:15 +0100

    [FEATURE]: added methods in Asset repository and Office repository

--- src/main/java/com/spx/inventory_management/repositories/AssetRepository.java
+++ src/main/java/com/spx/inventory_management/repositories/AssetRepository.java
@@ -1,4 +1,16 @@
 package com.spx.inventory_management.repositories;
 
-public interface AssetRepository {
+import com.spx.inventory_management.models.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AssetRepository extends JpaRepository <Asset, Long> {
+
+    boolean existsBySerialNumber(String serialNumber);
+
+    Optional<Asset> findBySerialNumber(String serialNumber);
+
+    void deleteBySerialNumber(String serialNumber);
+
 }
