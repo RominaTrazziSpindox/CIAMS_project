5273e1010e0edb716b58db698528261a1cca4244
Your Name <you@example.com>
2026-01-07 06:37:18 +0100

    [FEATURE]: added Software License model class

---
+++ src/main/java/com/spx/inventory_management/models/SoftwareLicense.java
@@ -0,0 +1,41 @@
+package com.spx.inventory_management.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "software_licenses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SoftwareLicense {
+
+    // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_software_license", nullable = false)
+    private long id;
+
+    @Column(name="software_name", nullable = false, unique = true)
+    private String softwareName;
+
+    @Column(name = "max_installations")
+    private int maxInstallations;
+
+    @Column(name="expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+
+    // Relation with Asset table on database
+    @ManyToMany
+    @JoinTable(
+            name = "assets_licenses", // Join table
+            joinColumns = @JoinColumn(name = "license_id"),
+            inverseJoinColumns = @JoinColumn(name = "asset_id")
+    )
+    private Set<Asset> installedAssets;
+}
