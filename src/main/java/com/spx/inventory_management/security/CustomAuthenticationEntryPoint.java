7975bfe595975b091359bae551b78ff21547a330
Your Name <you@example.com>
2026-01-08 11:49:38 +0100

    [FEATURE]: Spring Security pt.2: add Basic Auth and custom 401/403 handling

---
+++ src/main/java/com/spx/inventory_management/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,42 @@
+package com.spx.inventory_management.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spx.inventory_management.dto.ApiErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    // Constructor SpringBoot
+    public CustomAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request,HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        // Creating a new ApiResponseDTO
+        ApiErrorResponseDTO error = new ApiErrorResponseDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                "Unauthorized",
+                "Authentication required to access this resource",
+                "Provide valid authentication credentials",
+                "uri=" + request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        objectMapper.writeValue(response.getOutputStream(), error);
+    }
+}
