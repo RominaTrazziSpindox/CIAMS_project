b360ec179e1848414cabc1eab9f958eca1e0fab4
Your Name <you@example.com>
2026-01-08 11:49:38 +0100

    [FEATURE]: Spring Security pt.2: add Basic Auth and custom 401/403 handling

---
+++ src/main/java/com/spx/inventory_management/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,45 @@
+package com.spx.inventory_management.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spx.inventory_management.dto.ApiErrorResponseDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    // Constructor SpringBoot
+    public CustomAccessDeniedHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+
+        // Creating a new ApiResponseDTO
+        ApiErrorResponseDTO error = new ApiErrorResponseDTO(
+                HttpStatus.FORBIDDEN.value(),
+                "Forbidden",
+                "Access denied - insufficient permissions",
+                "Contact an administrator if access is required",
+                "uri=" + request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+        objectMapper.writeValue(response.getOutputStream(), error);
+    }
+
+}
+
--- src/main/java/com/spx/inventory_management/security/SecurityConfig.java
+++ src/main/java/com/spx/inventory_management/security/SecurityConfig.java
@@ -1,26 +1,43 @@
 package com.spx.inventory_management.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private CustomAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    private CustomAccessDeniedHandler accessDeniedHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
             // REST API → CSRF (Cross-Site Request Forgery) off
             .csrf(csrf -> csrf.disable())
 
             // No HTTP session (stateless)
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+
+            // AuthenticationEntryPoint (errors: 401 - Unauthorized & 403 - Forbidden permission )
+            .exceptionHandling(ex -> ex
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+            )
 
             // Authorization rules
             .authorizeHttpRequests(auth -> auth
@@ -28,17 +45,35 @@ public class SecurityConfig {
                 // WRITE operations require authentication
                 .requestMatchers(HttpMethod.POST, "/**").authenticated()
                 .requestMatchers(HttpMethod.PUT, "/**").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
 
                 // READ operations are public
                 .anyRequest().permitAll()
             )
 
-                // Basic Authentication
-                .httpBasic(Customizer.withDefaults());
+            // Basic Authentication
+            .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
 
+    // Type of users saved in RAM memory
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 }
+
+
+
 
