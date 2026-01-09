2f8f75a598d9f16d47ab03f09d3fbaa1dc1741fd
Your Name <you@example.com>
2026-01-09 13:44:09 +0100

    [FIX]: changed AllArgsContructor with NoArgsConstructor

--- src/main/java/com/spx/inventory_management/dto/SoftwareLicenseRequestDTO.java
+++ src/main/java/com/spx/inventory_management/dto/SoftwareLicenseRequestDTO.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class SoftwareLicenseRequestDTO implements Serializable {
 
     @NotBlank
