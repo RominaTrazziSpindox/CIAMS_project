f2952dff10d8ebfe1e2827a2518619015d7abacb
Your Name <you@example.com>
2026-01-12 04:27:00 +0100

    [FIX]: moved DTO mapping from Controller to Service for Asset entity

--- src/main/java/com/spx/inventory_management/repositories/AssetTypeRepository.java
+++ src/main/java/com/spx/inventory_management/repositories/AssetTypeRepository.java
@@ -4,10 +4,14 @@ import com.spx.inventory_management.models.AssetType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AssetTypeRepository extends JpaRepository<AssetType, Long> {
 
-    // DA FARE
-    boolean existsByAssetTypeName(String assetTypeName);
+    Optional<AssetType> findByAssetTypeNameIgnoreCase(String assetTypeName);
 
+    boolean existsByAssetTypeNameIgnoreCase(String assetTypeName);
+
+    void deleteByAssetTypeNameIgnoreCase(String assetTypeName);
 }
