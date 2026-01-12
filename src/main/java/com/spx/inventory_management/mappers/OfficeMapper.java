df041a32a0bb47cc02a23b339d58db25a4ad2c9f
Your Name <you@example.com>
2026-01-12 03:03:14 +0100

    [FIX]: moved DTO mapping from Controller to Service for Office entity

--- src/main/java/com/spx/inventory_management/mappers/OfficeMapper.java
+++ src/main/java/com/spx/inventory_management/mappers/OfficeMapper.java
@@ -12,10 +12,10 @@ public interface OfficeMapper {
 
     // DTO → Entity
     @Mapping(target = "id", ignore = true) // because client doesn't send the ID
-    @Mapping(target = "name", source = "officeName")
+    @Mapping(target = "name", source = "officeName") // target: db column, source: dto property
     Office toEntity(OfficeRequestDTO dto);
 
     // Entity → DTO
-    @Mapping(target = "officeName", source = "name")
+    @Mapping(target = "officeName", source = "name") // target; dto property, source: db column
     OfficeResponseDTO toDTO(Office entity);
 }
