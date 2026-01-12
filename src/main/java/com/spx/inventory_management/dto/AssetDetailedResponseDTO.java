5de08393db32978bbbcda6e72ebdab5dcdec1b70
Your Name <you@example.com>
2026-01-12 05:37:59 +0100

    [FEATURE]: added a more detailed ResponseDTO for asset class

--- src/main/java/com/spx/inventory_management/dto/AssetDetailedResponseDTO.java
+++ src/main/java/com/spx/inventory_management/dto/AssetDetailedResponseDTO.java
@@ -1,4 +1,18 @@
 package com.spx.inventory_management.dto;
 
-public class AssetDetailedResponseDTO {
+import lombok.Data;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+public class AssetDetailedResponseDTO implements Serializable {
+
+    private String serialNumber;
+    private LocalDate purchaseDate;
+
+    private OfficeResponseDTO office;
+    private AssetTypeResponseDTO assetType;
+
+    private Set<SoftwareLicenseResponseDTO> softwareLicenses;
 }
