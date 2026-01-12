3a9732d862b1eae28af94f2b59310f5d12ffcd32
Your Name <you@example.com>
2026-01-12 03:02:25 +0100

    [FEATURE]: added OfficeRequestNormalizer

---
+++ src/main/java/com/spx/inventory_management/utils/OfficeRequestNormalizer.java
@@ -0,0 +1,35 @@
+package com.spx.inventory_management.utils;
+
+import com.spx.inventory_management.dto.OfficeRequestDTO;
+
+/**
+ * Normalizes OfficeRequestDTO fields.
+ *
+ * This class is responsible ONLY for preparing incoming DTO data.
+ * No validation, no persistence, no business logic.
+ */
+
+public class OfficeRequestNormalizer {
+
+    // Constructor
+    private OfficeRequestNormalizer() {
+    }
+
+
+    // Method for fields normalization
+    public static OfficeRequestDTO normalize(OfficeRequestDTO inputOfficeRequestDTO) {
+
+        if (inputOfficeRequestDTO == null) {
+            return null;
+        }
+
+        OfficeRequestDTO normalized = new OfficeRequestDTO();
+
+        // Use of TextNormalizer function
+        normalized.setOfficeName(
+                TextNormalizer.normalizeKey(inputOfficeRequestDTO.getOfficeName())
+        );
+
+        return normalized;
+    }
+}
