bad2b901c9c856686e0c4006b45040bfd9d5a4f4
Your Name <you@example.com>
2026-01-16 13:18:22 +0100

    [CONFIG]: scope in-memory security users to dev profile only

--- src/main/java/com/spx/inventory_management/security/UserDetailsConfig.java
+++ src/main/java/com/spx/inventory_management/security/UserDetailsConfig.java
@@ -3,6 +3,7 @@ package com.spx.inventory_management.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Profile("dev")
 @Configuration
 public class UserDetailsConfig {
 
