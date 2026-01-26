f69fd64649eb017ab1089cd91ae9f1da9a1d5a31
Your Name <you@example.com>
2026-01-26 16:16:14 +0100

    [FEATURE]: added UserRepository interface with some methods

---
+++ auth-service/src/main/java/com/spx/auth_service/repositories/UserRepository.java
@@ -0,0 +1,33 @@
+package com.spx.auth_service.repositories;
+
+import com.spx.auth_service.models.Role;
+import com.spx.auth_service.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
+
+    // User Login
+    Optional<User> findByUsername(String username);
+
+    // User Register
+    boolean existsByUsername(String username);
+
+    // User Delete
+    void deleteByUsername(String username);
+
+
+    /* Further methods */
+
+    // Find users by role and paginate
+    Page<User> findByRoles(Role role, Pageable pageable);
+
+    boolean existsByRoles(Role role);
+
+
+}
