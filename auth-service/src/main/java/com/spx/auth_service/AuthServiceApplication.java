7e516da3e1f1f2266b1e4a5eaeea379ee792af6f
Your Name <you@example.com>
2026-01-28 11:50:48 +0100

    [CHORE]: added running message

--- auth-service/src/main/java/com/spx/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/spx/auth_service/AuthServiceApplication.java
@@ -1,13 +1,17 @@
 package com.spx.auth_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+		log.info("Auth service is running...");
+
 	}
 
 }
