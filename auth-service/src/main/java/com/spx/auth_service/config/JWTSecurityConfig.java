1dc559f99c2a1f6acd880be4e244e2342638bacc
Your Name <you@example.com>
2026-01-30 16:50:17 +0100

    [FEATURE]: added JWT SecurityConfig, JWT custom exceptions classes (401, 403)

---
+++ auth-service/src/main/java/com/spx/auth_service/config/JWTSecurityConfig.java
@@ -0,0 +1,83 @@
+package com.spx.auth_service.config;
+
+import com.spx.auth_service.security.JWTAccessDeniedHandler;
+import com.spx.auth_service.security.JWTAuthTokenFilter;
+import com.spx.auth_service.security.JWTAuthenticationEntryPoint;
+import com.spx.auth_service.services.JWTUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class JWTSecurityConfig {
+
+    @Autowired
+    private JWTUserDetailsService userDetailsService;
+    @Autowired
+    private JWTAuthenticationEntryPoint unauthorizedHandler;
+    @Autowired
+    private JWTAccessDeniedHandler deniedAccessHandler;
+
+
+    @Bean
+    public JWTAuthTokenFilter authenticationJwtTokenFilter() {
+        return new JWTAuthTokenFilter();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // REST API → CSRF (Cross-Site Request Forgery) off
+                .csrf(csrf -> csrf.disable())
+
+                // CORS OFF -> (Cross Origin Request Sharing) off
+                .cors( cors -> cors.disable())
+
+                // No HTTP session (stateless)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+
+                // AuthenticationEntryPoint (errors: 401 - Unauthorized & 403 - Forbidden permission )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(unauthorizedHandler)
+                        .accessDeniedHandler(deniedAccessHandler)
+                )
+
+                // Authorization rules
+                .authorizeHttpRequests(auth -> auth
+
+                        // WRITE operations require authentication
+                        .requestMatchers(HttpMethod.POST, "/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+
+                        // READ operations are public
+                        .anyRequest().permitAll()
+                )
+
+                // JWT Authentication filter
+                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                return http.build();
+
+        }
+
+
+    }
+
+}
